2b54f454f032dec11e50ec1e0114defaf4f7dddd
mwaney <emwaniki91@gmail.com>
2023-03-17 03:45:28 -0700

    7_edit

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 /** 
- * main : Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
-int main()
+int main(void)
 {
-    char c;
-
-    for (c = 'z'; c >= 'a'; c--)
-    {
-        putchar(c);
-    }
-    putchar('\n');
-
-    return (0);
+	char c;
+	
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
-

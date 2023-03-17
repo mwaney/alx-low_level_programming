5471e7efc5897bd03667ce774474e66d8296845e
mwaney <emwaniki91@gmail.com>
2023-03-17 04:19:49 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main: Prints numbers using putchar
+ * main- Prints numbers using putchar
  *
  * Return: 0 when successfull
  */
@@ -8,16 +8,17 @@ int main(void)
 {
 	int i;
 	int j;
-	for (i = 0; i<=99 ; i++)
+
+	for (i = 0; i <= 99; i++)
 	{
-		for(j=i+1; j<=99; j++)
+		for (j = i+1; j <= 99; j++)
 		{
 			putchar((i/10) + '0');
 			putchar((i%10) + '0');
 			putchar(' ');
 			putchar((j/10) + '0');
 			putchar((j%10) + '0');
-			if(!(i==98 && j ==99))
+			if (!(i==98 && j ==99))
 			{
 				putchar(',');
 				putchar(' ');
@@ -27,4 +28,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-

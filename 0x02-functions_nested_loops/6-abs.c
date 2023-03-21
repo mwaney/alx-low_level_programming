0dce008eab30c0ef5bb7a6872209888d480bddac
mwaney <emwaniki91@gmail.com>
2023-03-21 12:38:51 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  int _abs- function that tests if an alphabet is lowercase
+ * @int: The integer to test for
+ * Return: 1 if character is lowercase and 0 if otherwise
+ */
+int _abs(int)
+{
+	if (int < 0){
+		return int * -1;
+	}
+	else
+	{
+		return int;
+	}
+}

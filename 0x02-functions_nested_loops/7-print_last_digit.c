5b4501c624171d4d024cc3dae6a69e40378d42c2
mwaney <emwaniki91@gmail.com>
2023-03-21 12:51:33 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that tests if an alphabet is lowercase
+ * @n: The integer to test for
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int last = n % 10;
+
+	return (last);
+}

1ce856d00be22000227fa82e05e6dc130b230202
mwaney <emwaniki91@gmail.com>
2023-03-21 12:25:22 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - function that tests if an alphabet is lowercase
+ * @n: The integer to test for 
+ * Return: 1 if character is lowercase and 0 if otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}

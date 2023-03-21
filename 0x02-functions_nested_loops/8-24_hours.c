5a4586a6eba40535cf7b3594ebbd71adeb90c65b
mwaney <emwaniki91@gmail.com>
2023-03-21 13:26:05 -0700

    jack_bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer- function that tests if an alphabet is lowercase
+ * Return: last digit
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}

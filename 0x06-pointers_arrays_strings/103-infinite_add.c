856e4fded7608029f252d1ea182fdc70506a68bc
mwaney <emwaniki91@gmail.com>
2023-03-29 07:59:00 -0700

    103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,79 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
+ */
+
+void reverse(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+
+/**
+ * infinite_add - adds numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer that the function will use to store the result
+ * @size_r: buffer size
+ * Return: solution of the addition
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0, j = 0, carry = 0, digit = 0;
+	int num1 = 0, num2 = 0, sum = 0;
+
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || carry == 1)
+	{
+		if (i < 0)
+			num1 = 0;
+		else
+			num1 = *(n1 + i) - '0';
+		if (j < 0)
+			num2 = 0;
+		else
+			num2 = *(n2 + j) - '0';
+		sum = num1 + num2 + carry;
+		if (sum >= 10)
+			carry = 1;
+		else
+			carry = 0;
+		if (digit >= (size_r - 1))
+			return (0);
+		*(r + digit) = (sum % 10) + '0';
+		digit++;
+		i--;
+		j--;
+	}
+	if (digit == size_r)
+		return (0);
+	*(r + digit) = '\0';
+	reverse(r);
+	return (r);
+
+}

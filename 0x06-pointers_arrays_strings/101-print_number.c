2bc730711873b033ad9066f372536674349a9e15
mwaney <emwaniki91@gmail.com>
2023-03-29 06:03:46 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - function to print numbers
+ * @n: parameter to hold number
+ * Return: Nothing
+ */
+void print_number(int n)
+{
+	unsigned int num;
+
+	num = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	if (num / 10 != 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}

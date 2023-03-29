4db4c3d8d0410e15ddd5cfc2ebb5b891db3ed890
mwaney <emwaniki91@gmail.com>
2023-03-29 08:18:12 -0700

    Correction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,14 +6,19 @@
  * @src: source pointer
  * @n: limit of characters to add to first string
  * Return: an array of characters
+ *
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-       	int j = 0;
+	int i;
+	int j;
 
+	i = 0;
 	while (dest[i] != '\0')
+	{
 		i++;
+	}
+	j = 0;
 	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
@@ -23,3 +28,4 @@ char *_strncat(char *dest, char *src, int n)
 	dest[i] = '\0';
 	return (dest);
 }
+

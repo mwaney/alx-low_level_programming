d45d2cd7a53ccf6af698bd7009b2e7df8864c816
mwaney <emwaniki91@gmail.com>
2023-04-03 09:34:47 -0700

    use for loop

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,15 +7,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i = 0, j;
+	int i, j;
 
-	while (i < 8)
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
 		_putchar('\n');
-		i++;
 	}
 }

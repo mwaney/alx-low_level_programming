cf2a2880a0a17fb2d1959412daaf5e51aea69880
mwaney <emwaniki91@gmail.com>
2023-04-03 07:41:14 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strstr - function to find substring
+ * @haystack: String to search through
+ * @needle: what to search for
+ * Return: pointer to beggining of substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned char *h = (unsigned char *) haystack;
+	unsigned char *n = (unsigned char *) needle;
+	char *start;
+
+	while (*h)
+	{
+		start = haystack;
+		while(*n && *n == *h)
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+			return start;
+		h = (unsigned char *) ++haystack;
+		n = (unsigned char *) needle;
+	}
+	return ('\0');
+
+}

8990199c8a7adff4711e768006675d4f9e7bc838
mwaney <emwaniki91@gmail.com>
2023-04-05 03:10:19 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * helper - function that checks probability of a square root
+ * @n: number to get the square root from
+ * @s: start pointer
+ * @e: end pointer
+ * Return: the square root
+ */
+
+int helper(int n, int s, int e)
+{
+	int m = (s + e) / 2;
+	int square = m * m;
+
+	if (square == n)
+		return (m);
+	else if (s >= e)
+		return (e);
+	else if (square < n)
+		return (helper(n, m + 1, e));
+	else
+		return (helper(n, s, e - 1));
+}
+
+/**
+ * _sqrt_recursion - function that gives a square root
+ * @n: number to get the square root from
+ * Return: the square root
+ */
+int _sqrt_recursion(int n)
+{
+	int sqroot;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	sqroot = helper(n, 1, n / 2);
+
+	return (sqroot * sqroot == n ? sqroot : -1);
+}

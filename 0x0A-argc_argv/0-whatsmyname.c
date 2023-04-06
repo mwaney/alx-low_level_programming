0cd660950004b734d645b255a25e36d4ae698adb
mwaney <emwaniki91@gmail.com>
2023-04-06 05:45:36 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints out the name of the function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 Success
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}

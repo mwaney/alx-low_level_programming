f52aaad3ec02e057fd0556c85e9e488c74b40851
mwaney <emwaniki91@gmail.com>
2023-04-19 06:07:03 -0700

    correction

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,13 +20,9 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-	while (ops[i].op != NULL)
+	while (ops[i].op != NULL && s != *(ops[i].op))
 	{
-		if (*s == *(ops[i].op))
-		{
-			return (ops[i].f);
-		}
 		i++;
 	}
-	return (NULL);
+	return (ops[i].f);
 }

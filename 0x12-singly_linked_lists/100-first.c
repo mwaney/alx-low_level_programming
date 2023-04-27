2064660568b2d879305673a4b17647ba0bfc2b6d
mwaney <emwaniki91@gmail.com>
2023-04-27 04:12:24 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+
+/**
+ *  print_before- prints a sentence before execution of main
+ *  Return: Nothing
+ */
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+

75f939fb9a37015e320b2ae55d30b3f969ff0e23
mwaney <emwaniki91@gmail.com>
2023-04-29 05:48:17 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,28 +8,38 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const list_int_t *ptr1, *ptr2;
-	size_t i;
+	const listint_t *ptr1, *ptr2;
+	size_t i = 0;
 
 	if (!head)
 		exit(98);
 	ptr1 = head;
-	ptr2 = head->next;
+	ptr2 = head;
 
-	while (ptr2 && ptr2 != ptr1)
+	while (ptr1 && ptr2 && ptr2->next)
 	{
 		printf("[%p] %d\n", (void *) ptr1, ptr1->n);
-		i++;
 		ptr1 = ptr1->next;
-		ptr2 = ptr2->next;
+		ptr2 = ptr2->next->next;
+		i++;
 
-		if (ptr1)
-			ptr2 = ptr2->next;
+		if (ptr1 == ptr2)
+		{
+			printf("[%p] %d\n", (void *)ptr1, ptr1->n);
+			ptr1 = head;
+			while (ptr1 != ptr2)
+			{
+				printf("[%p] %d\n", (void *)ptr1, ptr1->n);
+				ptr1 = ptr1->next;
+				ptr2 = ptr2->next;
+				i++;
+			}
+			printf("-> [%p] %d\n", (void *) ptr1, ptr1->n);
+			return (i);
+		}
 	}
-	printf("[%p] %d\n", (void *) ptr1, ptr1->n);
+	printf("[%p] %d\n", (void *)ptr1, ptr1->n);
 	i++;
 
-	if (ptr1 == ptr2)
-		printf("-> [%p] %d\n", (void *) ptr2, ptr2->n);
 	return (i);
 }

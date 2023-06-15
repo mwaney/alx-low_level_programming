9d2ed29250d22c76d3d8d57944be92a5f1915dee
mwaney <emwaniki91@gmail.com>
2023-06-15 04:55:03 -0700

    103-keygen.c

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,51 +1,48 @@
+#include <string.h>
 #include "lists.h"
-
 /**
- * main - Function to print passwords for the crackme5 executable.
- * @argc: The argument count
- * @argv: Argument vector
+ * main - function generates a key for a given input
+ * @argcc: argument count.
+ * @argv: argument vector
  * Return: Always 0.
  */
-int main(__silent int argc, char *argv[])
+
+int main(__attribute__((unused))int argc, char *argv[])
 {
-	char password[7], *charset;
-	int input_length = strlen(argv[1]), i, tmp;
+	int i, temp;
+	int size = strlen(argv[1]);
+	char encr[7], *charset;
 
 	charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	tmp = (input_length ^ 59) & 63;
-	password[0] = charset[tmp];
+	temp = (size ^ 59) & 63;
+	encr[0] = charset[temp];
+	temp = 0;
+	for (i = 0; i < size; i++)
+		temp += argv[1][i];
+	encr[1] = charset[(temp ^ 79) & 63];
+	temp = 1;
+	for (i = 0; i < size; i++)
+		temp *= argv[1][i];
+	encr[2] = charset[(temp ^ 85) & 63];
 
-	tmp = 0;
-	for (i = 0; i < input_length; i++)
-		tmp += argv[1][i];
-	password[1] = charset[(tmp ^ 79) & 63];
-
-	tmp = 1;
-	for (i = 0; i < input_length; i++)
-		tmp *= argv[1][i];
-	password[2] = charset[(tmp ^ 85) & 63];
-
-	tmp = 0;
-	for (i = 0; i < input_length; i++)
+	temp = 0;
+	for (i = 0; i < size; i++)
 	{
-		if (argv[1][i] > tmp)
-			tmp = argv[1][i];
+		if (argv[1][i] > temp)
+			temp = argv[1][i];
 	}
-	srand(tmp ^ 14);
-	password[3] = charset[rand() & 63];
-
-	tmp = 0;
-	for (i = 0; i < input_length; i++)
-		tmp += (argv[1][i] * argv[1][i]);
-	password[4] = charset[(tmp ^ 239) & 63];
+	srand(temp ^ 14);
+	encr[3] = charset[rand() & 63];
+	temp = 0;
+	for (i = 0; i < size; i++)
+		temp += (argv[1][i] * argv[1][i]);
+	encr[4] = charset[(temp ^ 239) & 63];
 
 	for (i = 0; i < argv[1][0]; i++)
-		tmp = rand();
-	password[5] = charset[(tmp ^ 229) & 63];
-
-	password[6] = '\0';
-	printf("%s", password);
+		temp = rand();
+	encr[5] = charset[(temp ^ 229) & 63];
+	encr[6] = '\0';
+	printf("%s", encr);
 	return (0);
 }
-

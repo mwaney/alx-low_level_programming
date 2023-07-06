01c8ea1b78cde37fde14c2b9616898f76a92bf06
mwaney <emwaniki91@gmail.com>
2023-07-06 22:28:30 +0300

    Correction for task 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,27 +7,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_t = NULL;
+	hash_table_t *meza = NULL;
+	hash_node_t **lst = NULL;
 	unsigned long int i;
 
-	hash_t = malloc(sizeof(hash_table_t));
-	if (!hash_t)
+	meza = malloc(sizeof(hash_table_t));
+	if (!meza)
+		return (NULL);
+
+	meza->size = size;
+	lst = malloc(sizeof(hash_node_t *) * size);
+	if (lst == NULL)
 	{
 		return (NULL);
 	}
-
-	hash_t->array = malloc(size * sizeof(hash_node_t *));
-	if (hash_t->array == NULL)
-	{
-		free(hash_t);
-		return (NULL);
-	}
-
 	for (i = 0; i < size; i++)
-	{
-		hash_t->array[i] = NULL;
-	}
-	hash_t->size = size;
-	return (hash_t);
-
+		lst[i] = NULL;
+	meza->array = lst;
+	return (meza);
 }

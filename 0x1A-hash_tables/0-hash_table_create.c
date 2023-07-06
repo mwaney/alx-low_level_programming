112dff17d2ca35fe8022aa152293d52a2e5f2d4f
mwaney <emwaniki91@gmail.com>
2023-07-06 22:09:25 +0300

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_t;
 	unsigned long int i;
+	hash_node_t **lst = NULL;
 
 	hash_t = malloc(sizeof(hash_table_t));
 	if (!hash_t)
@@ -16,18 +17,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 
-	hash_t->array = malloc(size * sizeof(hash_node_t *));
-	if (hash_t->array == NULL)
+	hash_t->size = size;
+	lst = malloc(size * sizeof(hash_node_t *));
+	if (lst == NULL)
 	{
-		free(hash_t);
 		return (NULL);
 	}
 
 	for (i = 0; i < size; i++)
 	{
-		hash_t->array[i] = NULL;
+		lst[i] = NULL;
 	}
-	hash_t->size = size;
+	hash_t->array = lst;
 	return (hash_t);
 
 }
